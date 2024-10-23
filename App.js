4feb7a7e231a5b41ef6cd17317ce0167d31e2032
Zhi Xin <23009854@myrp.edu.sg>
2024-10-23 14:11:22 +0800

    L2_lesson

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
+import { ScrollView } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome6" ;
+import { Image } from 'react-native';
 
-export default function App() {
+const Movie = ({title, year, icon_name, poster})=> {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ScrollView>
+          <Image
+              source={poster}
+              style={{ width: 400, height: 500 }}
+          />
+          <Text>{title} {year}</Text>
+          <Icon name={icon_name} size={20} color="#B23B23" />
+      </ScrollView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AllMovies = () => {
+    return (
+        <ScrollView>
+            <Movie
+                title="Doctor Sleep"
+                year="2019"
+                poster={require('./img/doctor-sleep.jpg')}
+                icon_name="skull"
+            />
+            <Movie
+                title="Midway"
+                year="2020"
+                poster={require('./img/midway.jpg')}
+                icon_name="person-rifle"
+            />
+        </ScrollView>
+    );
+};
+
+const App = () => {
+    return (
+        <ScrollView>
+            <AllMovies />
+        </ScrollView>
+    );
+};
+export default App;
